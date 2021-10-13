57c2375ff1a46bb9b70d3d7367e08b01ba344d04
BanisBT <banisbt@gmail.com>
2021-10-13 10:29:35 +0300

    First commit

---
+++ src/main/java/com/tbarauskas/elastumtask/ElastumTaskApplication.java
@@ -0,0 +1,13 @@
+package com.tbarauskas.elastumtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElastumTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElastumTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tbarauskas/elastumtask/ElastumTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbarauskas.elastumtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElastumTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
